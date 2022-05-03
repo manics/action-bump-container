0fbe6940899c60c68d36dcac233bc508a4a2b58e
Simon Li <orpheus+devel@gmail.com>
2022-05-03 22:36:41 +0100

    WIP: main.ts run()

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,37 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+// import { mainModule } from 'process'
+import * as tags from './tags'
+
+// async function run(): Promise<void> {
+//   try {
+//     const ms: string = core.getInput('milliseconds')
+//     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+
+//     core.debug(new Date().toTimeString())
+//     await wait(parseInt(ms, 10))
+//     core.debug(new Date().toTimeString())
+
+//     core.setOutput('time', new Date().toTimeString())
+//   } catch (error) {
+//     if (error instanceof Error) core.setFailed(error.message)
+//   }
+// }
 
 async function run(): Promise<void> {
+  const dt = await tags.dockerHubGetTag('openmicroscopy/omero-server', 'latest')
+  core.info(JSON.stringify(dt, null, 2))
+
+  const qt = await tags.quayIoGetTag('jupyterhub/repo2docker', 'main')
+  core.info(JSON.stringify(qt, null, 2))
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const xt = await tags.dockerHubListTags('openmicroscopy/omero-server/xxx')
+    core.info(JSON.stringify(xt, null, 2))
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.error(error.message)
+      // core.setFailed(error.message)
+    }
   }
 }
 
