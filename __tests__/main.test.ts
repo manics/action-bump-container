075e723dbfcc79d3ed04185afdf493449bd8d03f
Simon Li <orpheus+devel@gmail.com>
2022-06-12 23:27:41 +0100

    Add update file support to main

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,25 +1,36 @@
-import {expect, test} from '@jest/globals'
-import nock from 'nock'
+import {afterEach, beforeEach, expect, jest, test} from '@jest/globals'
+import {readFileSync, writeFileSync} from 'fs'
 import * as process from 'process'
 import * as child_process from 'child_process'
 import * as path from 'path'
+import * as utils from './utils'
 
-test('throws 1 equals 1', async () => {
-  expect(1).toEqual(1)
+jest.retryTimes(0)
+
+let tmpFile: string
+
+beforeEach(() => {
+  tmpFile = utils.createTmpFile()
 })
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test.each([
+afterEach(() => {
+  utils.deleteFile(tmpFile)
+})
+
+const containerRepos = [
   ['docker.io/openmicroscopy/omero-web', 'latest'],
   ['quay.io/jupyterhub/repo2docker', 'main']
-])('run %s', (repo, pointer) => {
+]
+const np = process.execPath
+const ip = path.join(__dirname, '..', 'lib', 'main.js')
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test.each(containerRepos)('run tags %s', (repo, pointer) => {
   process.env['INPUT_REPO'] = repo
   process.env['INPUT_POINTERTAG'] = pointer
   // process.env['INPUT_REGEX'] = '^\\d+\\.\\d+\\.\\d+$'
   process.env['INPUT_MAXTAGSTOFETCH'] = '10'
 
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: child_process.ExecFileSyncOptions = {
     env: process.env
   }
@@ -44,4 +55,88 @@ test.each([
   expect(
     outputs[0].match('^::set-output name=tag::\\d+\\.\\d+\\.\\d+(-\\S+)?$')
   ).toBeTruthy()
+})
+
+test.each([containerRepos[1]])('run update files %s', (repo, pointer) => {
+  process.env['INPUT_REPO'] = repo
+  process.env['INPUT_POINTERTAG'] = pointer
+  // process.env['INPUT_REGEX'] = '^\\d+\\.\\d+\\.\\d+$'
+  process.env['INPUT_MAXTAGSTOFETCH'] = '10'
+
+  process.env['INPUT_UPDATEFILE'] = tmpFile
+  process.env['INPUT_PATTERN'] =
+    '(repository: quay\\.io\\/jupyterhub\\/repo2docker\\n\\s+tag:) (\\S+)'
+  process.env['INPUT_REPLACE'] = '$1 ${tag}'
+
+  const valuesYml = utils.loadValuesYml()
+  const input = valuesYml.replace('%TAG_R2D%', '0.0.1')
+  writeFileSync(tmpFile, input, 'utf8')
+  console.debug(`Using tmpFile: ${tmpFile}`)
+
+  const options: child_process.ExecFileSyncOptions = {
+    env: process.env
+  }
+
+  let stdout
+
+  // Use try-catch so that we can display errors if the child process fails
+  try {
+    stdout = child_process.execFileSync(np, [ip], options).toString()
+  } catch (err) {
+    const e = err as child_process.SpawnSyncReturns<Buffer>
+    console.log(`status: ${e.status}`)
+    console.error(`stdout: ${e.stdout.toString()}`)
+    console.error(`stderr: ${e.stderr.toString()}`)
+    throw e
+  }
+  console.log(stdout)
+  const outputs = stdout
+    .split('\n')
+    .filter(line => line.startsWith('::set-output '))
+  expect(outputs.length).toEqual(2)
+  const m = outputs[0].match(
+    '^::set-output name=tag::(\\d+\\.\\d+\\.\\d+(-\\S+)?)$'
+  )
+  expect(m).toBeTruthy()
+  expect(m!.length).toBe(3)
+  const tag = m![1]
+  console.debug(`tag: ${tag}`)
+  expect(tag.length).toBeGreaterThanOrEqual(5)
+
+  expect(outputs[1]).toBe('::set-output name=updated::true')
+
+  const expected = valuesYml.replace('%TAG_R2D%', tag)
+  const updatedContent = readFileSync(tmpFile, 'utf8')
+  expect(updatedContent).toEqual(expected)
+  expect(
+    updatedContent.includes(
+      `r2d:\n  image:\n    repository: quay.io/jupyterhub/repo2docker\n    tag: ${tag}\n`
+    )
+  ).toBeTruthy()
+})
+
+test('incorrect input', () => {
+  process.env['INPUT_REPO'] = 'ignored'
+  process.env['INPUT_POINTERTAG'] = 'ignored'
+  process.env['INPUT_MAXTAGSTOFETCH'] = '10'
+
+  process.env['INPUT_UPDATEFILE'] = '10'
+  process.env['INPUT_PATTERN'] = 'non-empty'
+  process.env['INPUT_REPLACE'] = ''
+
+  const options: child_process.ExecFileSyncOptions = {
+    env: process.env
+  }
+
+  const expectedError =
+    '::error::updateFile, pattern, and replace must be used together'
+
+  expect.assertions(1)
+
+  try {
+    child_process.execFileSync(np, [ip], options).toString()
+  } catch (err) {
+    const e = err as child_process.SpawnSyncReturns<Buffer>
+    expect(e.stdout.toString().trim()).toBe(expectedError)
+  }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as tags from './tags'
+import * as update from './update'
 
 async function run(): Promise<void> {
   try {
@@ -7,6 +8,14 @@ async function run(): Promise<void> {
     const pointer: string = core.getInput('pointerTag')
     const regex: string = core.getInput('regex')
     const maxTagsToFetch: number = parseInt(core.getInput('maxTagsToFetch'))
+
+    const updateFile: string = core.getInput('updateFile')
+    const pattern: string = core.getInput('pattern')
+    const replace: string = core.getInput('replace')
+
+    if (!!updateFile !== !!pattern || !!updateFile !== !!replace) {
+      throw new Error('updateFile, pattern, and replace must be used together')
+    }
 
     const dockerMatch = repo.match('^docker\\.io/(.+)')
     const quayMatch = repo.match('^quay\\.io/(.+)')
@@ -24,6 +33,15 @@ async function run(): Promise<void> {
     const tag = await tags.getMatchingTag(tagList, pointerTag, regex)
     core.debug(`Found ${tag.toString()}`)
     core.setOutput('tag', tag.tag)
+
+    if (updateFile) {
+      const updated = update.updateFile(
+        updateFile,
+        pattern,
+        replace.replace('${tag}', tag.tag)
+      )
+      core.setOutput('updated', updated)
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
